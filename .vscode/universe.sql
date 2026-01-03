4509282794c74393a2e490db4f23be645cd33c19
anithraradhakrishnan <anithraradhakrishnan@gmail.com>
2026-01-03 20:07:59 +0530

    Create universe.sql

---
+++ .vscode/universe.sql
@@ -0,0 +1,19 @@
+-- Create galaxy table
+CREATE TABLE galaxy (...);
+INSERT INTO galaxy (...);
+
+-- Create star table
+CREATE TABLE star (...);
+INSERT INTO star (...);
+
+-- Create planet table
+CREATE TABLE planet (...);
+INSERT INTO planet (...);
+
+-- Create moon table
+CREATE TABLE moon (...);
+INSERT INTO moon (...);
+
+-- Create comet table
+CREATE TABLE comet (...);
+INSERT INTO comet (...);
